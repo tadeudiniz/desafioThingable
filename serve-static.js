a8938816ed956c9e87868957c6f11987a982eb92
Tadeu Diniz <luiztadeudiniz@gmail.com>
2021-08-22 01:43:58 -0300

    Adiciona fallback para index.html

--- serve-static.js
+++ serve-static.js
@@ -1,5 +1,9 @@
+const fallback = require('express-history-api-fallback')
 const express = require('express')
 const app = express()
 const root = `${__dirname}/dist/desafio`
+
 app.use(express.static(root))
+app.use(fallback('index.html', { root }))
+
 app.listen(process.env.PORT || 9999)
