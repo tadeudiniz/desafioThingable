8551ea6cdfac62c02f4941f9e5d567f0aae53448
Tadeu Diniz <luiztadeudiniz@gmail.com>
2021-08-22 00:40:22 -0300

    Usa a variável de ambiente PORT

--- server.js
+++ server.js
@@ -118,6 +118,6 @@ server.use(/^(?!\/auth).*$/,  (req, res, next) => {
 
 server.use(router)
 
-server.listen(8000, () => {
+server.listen(process.env.PORT || 8000, () => {
   console.log('Run Auth API Server')
 })
